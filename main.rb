d35122ebdc29c0d0e9736b0f92118aa0813be13e
Obliterati <obliteratedinventory@gmail.com>
2016-04-05 17:23:14 +0100

    now removes forts etc. as EG balancing policy

--- main.rb
+++ main.rb
@@ -14,6 +14,12 @@ def file_cleaner(filename)
       safe_text = safe_text.slice!(0..first_history_line_number)
     end
   end
+  safe_text.each_with_index do |line, index| 
+    if line =~ /add_local_autonomy =/ || line =~ /estate/ || line =~ /fort_15th/ || line =~ /unrest/
+      puts 'found minor info'
+      safe_text = safe_text.slice(0..index-1) + safe_text.slice(index..-1)
+    end
+  end
   File.open(filename, 'w+') {|f| f.puts safe_text}
 end
 
@@ -21,7 +27,7 @@ def file_reader(filename)
   begin
     file = File.open(filename).drop(0)
   rescue SystemCallError
-    puts "inside file_reader, #{filename}"
+    puts "No corresponding file for #{filename}"
     file = File.open(old_filename(filename)).drop(0)
   end
   safe_text = file.each { |line| line.encode!(invalid: :replace)}
@@ -29,19 +35,22 @@ def file_reader(filename)
 end
 
 def cores_getter(text)
-  # safe_text = file_reader(filename)
   added_cores = text.select { |line| line =~ /(add_core = .{3})/ }
   tag_strings = added_cores.map { |core| core.to_s.split('=') }.flatten.select { |chunk| chunk =~ /([A-Z]{3})/}.map {|string| string.strip}
   tags = tag_strings.map {|tag| tag[0..2]}
 end
 
 def tag_getter(text)
-  # safe_text = file_reader(filename)
-  tag = text.find { |line| line =~ /(owner = .{3})/ }.chomp.split[2]
+ tag = text.find { |line| line =~ /(add_core = .{3})/ }
+ if !tag
+  tag = 'NIL'
+else
+  tag = tag.chomp.split[2]
+end
+tag
 end
 
 def getter(text, string)
-  # safe_text = file_reader(filename)
   setting = text.find { |line| line =~ /#{string}/}
   if setting != nil
     setting
@@ -70,12 +79,9 @@ def history_string(filename)
  vanilla_tag = tag_getter(original_text)
  eg_tag = tag_getter(eg_text)
 
- extra_tags = cores_getter(original_text)
-  #{extra_tags.map {|tag| "add_core = #{tag}\n"}.join}
+  history_string = "1445.1.2 = {\nremove_core = #{ vanilla_tag }\nadd_core = #{ eg_tag }\ncontroller = #{ eg_tag }\nowner = #{ eg_tag }\n#{ getter(eg_text, 'religion') }#{ getter(eg_text, 'culture') }} # EG/vanilla merge added by Obliterati with EGMapEdit"
 
-  history = "1445.1.2 = {\nremove_core = #{ vanilla_tag }\ncontroller = #{ eg_tag }\nowner = #{ eg_tag }\n#{ getter(eg_text, 'religion') }#{ getter(eg_text, 'culture') }\n  } # EG/vanilla merge added by Obliterati with EGMapEdit"
-
-  # YOU NOW NEED TO ACCOUNT FOR DEVELOPMENT ALTERATIONS
+  # INSTEAD, REMOVE THESE MINOR DETAILS FROM ALL FILES
 end
 
 def full_writer(filename)
@@ -94,23 +100,25 @@ def full_run(directory)
     rescue SystemCallError
       puts "FAILED: #{file} - SystemCallError"
       system_call_errors += 1
-    # possible causes include that there is no new, edited analogue of a file
-    next
-  rescue NoMethodError
-    puts "FAILED: #{file} - NoMethodError"
-    no_method_errors += 1
-    next
-  rescue ArgumentError
-    puts "FAILED: #{file} - ArgumentError"
-    argument_errors += 1
-    next
+      next
+    rescue NoMethodError
+      puts "FAILED: #{file} - NoMethodError"
+      no_method_errors += 1
+      next
+    rescue ArgumentError
+      puts "FAILED: #{file} - ArgumentError"
+      argument_errors += 1
+      next
+    end
   end
-  end
-  count = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count
+  count = Dir.glob(File.join(directory, '**', '*')).select { |file| File.file?(file) }.count - 3
   puts "all files merged.  Errors as follows:\nSystemCallErrors: #{system_call_errors}\nNoMethodErrors: #{no_method_errors}\nArgumentErrors: #{argument_errors}\nof a total #{count} files"
 end
 
-# full_writer("history_output/provinces/10-Jamtland.txt")
+# file_cleaner("history/provinces/117 - Siena.txt")
+# full_writer("history_output/provinces/117 - Siena.txt")
+
+# ACTUAL RUN
 
 puts 'cleaning files...'
 Dir.foreach("history_output/provinces") do |file|
